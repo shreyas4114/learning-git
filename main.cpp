66aa4ef8bcd35e9c0349472bf2be9a8c138420ea
shreyas4114 <125130119+shreyas4114@users.noreply.github.com>
2023-08-09 20:33:55 +0530

    modified main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// This program reverses the array 
+
 #include<iostream>
 using namespace std;
 
@@ -13,3 +15,27 @@ void reverse(int arr[], int n)     // reverse the array
         end--;
     }  
 }
+
+void printarr(int arr[], int n)       // printf the array
+{
+    for (int i = 0; i <= n-1; i++)
+    {
+        cout << arr[i] << " ";
+    }    
+    cout << endl;
+}
+                             
+int main()
+{
+    int arr1[5] = {1,2,3,4,5};
+    int arr2[6] = {12,33,21,5,41,57};
+
+    reverse(arr1, 5);
+    reverse(arr2, 6);
+
+    printarr(arr1, 5);
+    printarr(arr2, 6);
+
+    return 0;
+}
+
